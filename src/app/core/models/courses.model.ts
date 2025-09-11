a47f9642d219d17fe3f4a3b13e064a8f8a4118c2
Jeffer <jeffersondavila1101@gmail.com>
2025-09-10 21:42:23 -0500

    Courses
    Add connection to endpoint

--- src/app/core/models/courses.model.ts
+++ src/app/core/models/courses.model.ts
@@ -1,10 +1,26 @@
+
 export interface Course {
-    uniqueID: string;
-    name: string;
-    description: string;
-    tittle: string;
-    level: string;
-    urlImage: string;
-    categoryID: string;
-    teacherUserID: string;
+  uniqueID: string;
+  name: string;
+  description: string;
+  title: string; // corregido
+  level: string;
+  url_image: string; // mantener snake_case si no usas transformaciones
+  category_id: string;
+  teachaer_user_id: string; // mantener como lo manda el backend
+  teacher: Teacher;
+}
+
+export interface Teacher {
+  uniqueID: string;
+  primer_nombre: string;
+  segundo_nombre: string;
+  primer_apellido: string;
+  segundo_apellido: string;
+  cod_estudiante: string;
+  email: string;
+  has_password: string;
+  faculty_uuid: string;
+  rol_uuid: string;
+  id_password_reset: string | null;
 }
